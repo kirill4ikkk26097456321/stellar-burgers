8b1dd5f41174ac3c50465dada1012f64026e42bf
Комарницкий Кирилл Иванович <kikomarnitskii@edu.hse.ru>
2026-06-06 18:24:05 +0300

    fixed1

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,5 +1,4 @@
 import { FC, SyntheticEvent, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { RegisterUI } from '@ui-pages';
 import { useDispatch, useSelector } from '../../services/store';
 import {
@@ -14,19 +13,13 @@ export const Register: FC = () => {
   const [password, setPassword] = useState('');
 
   const dispatch = useDispatch();
-  const navigate = useNavigate();
 
   const error = useSelector(selectUserError);
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
 
-    dispatch(registerUser({ name: userName, email, password }))
-      .unwrap()
-      .then(() => {
-        navigate('/', { replace: true });
-      })
-      .catch(() => {});
+    dispatch(registerUser({ name: userName, email, password }));
   };
 
   return (
